bbb2ed286c14228052f0664a4a1456841cc49a65
Mateus Tudeia <mateustudeia@gmail.com>
2020-09-25 12:13:53 -0300

    criando a configuração da aplicação com express e mapeando as rotas

---
+++ src/routes/routes.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import FilmeController from '../controllers/FilmeController';
+import UsuarioController from '../controllers/UsuarioController';
+
+const routes = express.Router();
+const usuarioControllers = new UsuarioController(); 
+const filmeControllers = new FilmeController();
+
+routes.post('/usuario', usuarioControllers.create);
+
+routes.get('/filme', filmeControllers.index);
+routes.post('/filme', filmeControllers.create);
+
+
+export default routes;
