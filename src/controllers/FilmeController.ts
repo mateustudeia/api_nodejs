24ea67cb505123e50db99c57478d5949b11f3249
Mateus Tudeia <mateustudeia@gmail.com>
2020-09-27 11:19:59 -0300

    Correção da requisição de filmes com filtro

--- src/controllers/FilmeController.ts
+++ src/controllers/FilmeController.ts
@@ -1,22 +1,15 @@
 import { Request, Response } from  'express';
 import db from '../database/connection';
 
-interface FilmeModel {
-    nome: string;
-    nome_diretor: string;
-    genero: string;
-    atores: string;
-}
-
 export default class FilmeController {
 
     async index(request: Request, response: Response) {
         const filters = request.query;
 
-        const nome = filters.name as string;
+        const nome = filters.nome as string;
         const nome_diretor = filters.nome_diretor as string;
         const genero = filters.genero as string;
-        const atores = filters.atores as string;
+        const atores = filters.atores as string;    
 
         if (!filters.nome && !filters.nome_diretor && !filters.genero && !filters.atores) {
             return response.status(400).json({
@@ -25,13 +18,13 @@ export default class FilmeController {
         }
 
         const filmes = await db('filme')
-            .where('`filme`.`nome` = ??', [String(nome)]) 
-            .or.where('`filme`.`nome_diretor` = ??', [String(nome_diretor)])
-            .or.where('`filme`.`genero` = ??', [String(genero)])
-            .or.where('`filme`.`atores` = ??', [String(atores)])
-            .select(['filme.*']);
+            .where('filme.nome', '=', String(nome)) 
+            .orWhere('nome_diretor', String(nome_diretor))
+            .orWhere('genero', String(genero))
+            .orWhere('atores', String(atores))
+            .select('*')
         
-        
+        console.log(filmes);
         return response.json(filmes) 
     }
     async create (request: Request, response: Response) {
